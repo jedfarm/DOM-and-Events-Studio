9f028d96c5b6b2cb498dcf7a7c0d8d766bbf38f3
jedfarm <jedfarm@blackhorse.lan>
2020-05-06 20:11:07 -0400

    added functionality for up, down, left and right buttons, id for the corresponding html tags added.

--- scripts.js
+++ scripts.js
@@ -12,12 +12,18 @@ window.addEventListener("load", function(){
     let shuttleBackground = this.document.getElementById("shuttleBackground");
     let shuttleHeight = this.document.getElementById("spaceShuttleHeight");
 
+    let imgObj = document.getElementById('rocket');
+    imgObj.style.position= 'absolute'; 
+    imgObj.style.left = '0px'; 
+    imgObj.style.bottom = '0px';
+
     btTakeOff.addEventListener("click", function(){
         let result = window.confirm("Confirm that the shuttle is ready to takeoff");
         if (result) {
             flightStatus.innerHTML = "Shuttle in flight";
             shuttleBackground.style.backgroundColor = "blue";
             shuttleHeight.innerHTML = "10000";
+            imgObj.style.bottom = 10 +"px";
         }
     });
 
@@ -26,6 +32,7 @@ window.addEventListener("load", function(){
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
         shuttleHeight.innerHTML = "0";
+        imgObj.style.bottom = 0 +"px";
     });
 
     btAbortMission.addEventListener("click", function(){
@@ -34,9 +41,38 @@ window.addEventListener("load", function(){
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style.backgroundColor = "green";
             shuttleHeight.innerHTML = "0";
+            imgObj.style.bottom = 0 +"px";
         }
     });
 
+
+    
+
+    let right = this.document.getElementById('right');
+    right.addEventListener("click", function () {
+        movement = parseInt(imgObj.style.left) + 10 + 'px';
+        imgObj.style.left = movement;
+        //flightStatus.innerHTML = "Move to the Right!";
+    });
+
+    let left = this.document.getElementById('left');
+    left.addEventListener("click", function () {
+        movement = parseInt(imgObj.style.left) - 10 +'px';
+        imgObj.style.left = movement;
+    });
+
+    let down = this.document.getElementById('down');
+    down.addEventListener("click", function () {
+        movement = parseInt(imgObj.style.bottom) - 10 + 'px';
+        imgObj.style.bottom = movement;
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) - 10000;
+    });
+    let up = this.document.getElementById('up');
+    up.addEventListener("click", function () {
+        movement = parseInt(imgObj.style.bottom) + 10 + 'px';
+        imgObj.style.bottom = movement;
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
+    });
 
 });    
 
